b1302dd928b81602568cad0aaddcc09d750129b6
Alex Ch <churkindev@gmail.com>
2022-08-29 19:08:56 +0300

    Top buttons styled

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -1,11 +1,17 @@
-import { Navbar, Container, Row, Col, ToggleButtonGroup, ToggleButton, NavbarBrand } from 'react-bootstrap';
+import {
+    Navbar,
+    Container,
+    Row, Col,
+    ToggleButtonGroup,
+    ToggleButton, NavbarBrand
+} from 'react-bootstrap';
 
 const Navibar = () => (
     <Container className="pb-3">
         <Row>
             <Navbar>
                 <NavbarBrand className="mx-auto">
-                    <span class="h2">CryptoPrices</span>
+                    <span className="nav-header-text">CryptoPrices</span>
                 </NavbarBrand>
             </Navbar>
 
@@ -15,28 +21,28 @@ const Navibar = () => (
             <Col>
                 <ToggleButtonGroup
                     type="radio"
-                    exclusive
+                    exclusive="true"
                     name="options"
                     defaultValue={1}>
 
                     <ToggleButton id="tbg-b-1" variant="outline-primary" size="lg" className="nav-button shadow-none" value={1}>
-                        <span class="nav-button-text">₽</span>
+                        <span className="nav-button-text">₽</span>
                     </ToggleButton>
 
-                    <ToggleButton id="tbg-b-2" variant="outline-primary" size="lg" className="nav-button shadow-none" value={2}>
-                        <span class="nav-button-text">$</span>
+                    <ToggleButton id="tbg-b-2" variant="outline-primary" size="lg" className="nav-button shadow-none btn-dark" value={2}>
+                        <span className="nav-button-text">$</span>
                     </ToggleButton>
 
                     <ToggleButton id="tbg-b-3" variant="outline-primary" size="lg" className="nav-button shadow-none" value={3}>
-                        <span class="nav-button-text">€</span>
+                        <span className="nav-button-text">€</span>
                     </ToggleButton>
 
                     <ToggleButton id="tbg-b-4" variant="outline-primary" size="lg" className="nav-button shadow-none" value={4}>
-                        <span class="nav-button-text">£</span>
+                        <span className="nav-button-text">£</span>
                     </ToggleButton>
 
                     <ToggleButton id="tbg-b-5" variant="outline-primary" size="lg" className="nav-button shadow-none" value={5}>
-                        <span class="nav-button-text">₽</span>
+                        <span className="nav-button-text">¥</span>
                     </ToggleButton>
                 </ToggleButtonGroup>
             </Col>
