5936e2ec541a4d62317134e820237e9496d5c6a8
= <churkindev@gmail.com>
2022-09-04 12:58:30 +0300

    ListItem is ready

--- src/Components/MasterView.js
+++ src/Components/MasterView.js
@@ -12,9 +12,40 @@ const MasterView = () => (
     <Container className={`${styles.text} ${styles.masterViewContainer} px-0 pb-3`}>
         <CustomInput placeholder="Поиск криптовалют..." />
         <div className="list-group">
-            <ListItem rank={1} name={"Bitcoin"} price={20000} currency={'usd'} iconUrl={"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"} />
-            <ListItem rank={2} name={"Bitcoin"} price={20000} currency={'eur'} iconUrl={"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"} />
-            <ListItem rank={3000} name={"Bitcoin"} price={20000} currency={'rub'} iconUrl={"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"} />
+            <ListItem
+                iconUrl={"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"}
+                name={"Bitcoin"}
+                rank={1}
+                price={20000000}
+                currency={'usd'}
+                change24h={-0.1}
+
+                volume24h={23432439543544}
+                mktCap={3549545845495458394530943}
+            />
+
+            <ListItem
+                iconUrl={"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"}
+                name={"Bitcoin"}
+                rank={1}
+                price={5000}
+                currency={'eur'}
+                change24h={-0.12}
+
+                volume24h={23432439543544}
+                mktCap={354954394530943}
+            />
+
+            <ListItem
+                iconUrl={"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"}
+                name={"Bitcoin"}
+                rank={1}
+                price={100}
+                currency={'rub'}
+
+                change24h={12.00}
+                mktCap={354954394530943}
+            />
         </div>
     </Container>
 )
--- src/Components/ui/ListItem.js
+++ src/Components/ui/ListItem.js
@@ -17,13 +17,25 @@ function getSignForCurrency(currency) {
     }
 }
 
-const ListItem = ({ iconUrl, name, rank, price, currency /*, change24h, volume24h, mktCap*/ }) => (
+const ListItem = ({ iconUrl, name, rank, price, currency, change24h, volume24h, mktCap }) => (
     <a href="#" className={`${styles.commonItem} list-group-item list-group-item-action align-items-start`} data-toggle="list">
         <div className={`${styles.part1} pb-0`}>
             <img className={`${styles.icon} my-auto`} src={iconUrl} alt={name} />
             <h5 className={`my-auto`}>{name} {(rank != null) ? `(#${rank})` : ""} </h5>
         </div>
-        <div className={`${styles.part2}`}>{getSignForCurrency(currency)} {price}</div>
+
+        <div className={`d-flex flex-row ${styles.part2} justify-content-between`}>
+            <div className={` d-flex`}>
+                {currency !== 'rub' ? getSignForCurrency(currency) : ''} {price} {currency === 'rub' ? getSignForCurrency(currency) + ' ' : ''}
+            </div>
+            <div>
+                <span className={(change24h > 0.0) ? `${styles.textGreen}` : `${styles.textRed}`}>{change24h.toFixed(2)}%</span>
+            </div>
+        </div>
+        <div className={`d-flex flex-row ${styles.part2} justify-content-between`}>
+            Объём (24ч): {currency !== 'rub' ? getSignForCurrency(currency) : ''} {volume24h} {currency === 'rub' ? getSignForCurrency(currency) + ' ' : ''}<br />
+            Р. кап.: {currency !== 'rub' ? getSignForCurrency(currency) : ''} {mktCap} {currency === 'rub' ? getSignForCurrency(currency) + ' ' : ''}
+        </div>
     </a>
 )
 
