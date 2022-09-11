0321f8703c99f6f39189ebb47706a5219a6c7cad
= <churkindev@gmail.com>
2022-09-11 18:30:34 +0300

    Some warnings fixed

--- src/components/ui/CustomInput.js
+++ src/components/ui/CustomInput.js
@@ -1,5 +1,4 @@
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
-import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadSearchHintsAction } from '../../asyncActions/CoinGeckoActions';
 import { useNavigate } from 'react-router-dom';
@@ -9,25 +8,15 @@ const CustomInput = (props) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const [emptyLabel, setEmptyLabel] = useState([]);
-
-    const isHintsLoading = useSelector(state => state.search.isLoading);
     const hints = useSelector(state => state.search.hints);
-
-    /*useEffect(() => {
-        if (isHintsLoading)
-            setEmptyLabel("Загрузка...");
-        else
-            setEmptyLabel("Ничего не найдено.");
-    }, [isHintsLoading]);*/
 
     return (
         <AsyncTypeahead
             id="asyncSearchCoins"
-            placeholder="Поиск криптовалют..."
-            labelKey="name"
-            emptyLabel="Загрузка..."
             className={`customInput mt-3 mb-3`}
+            placeholder="Поиск криптовалют..."
+            emptyLabel="Загрузка..."
+            labelKey="name"
             isLoading={false}
             onSearch={(query) => {
                 console.log("search text: " + query);
@@ -38,9 +27,9 @@ const CustomInput = (props) => {
             onChange={(selected) => {
                 console.log("search: element selected:");
                 console.log(selected);
-                if(selected[0] != undefined)
+                if(selected[0] !== undefined)
                     navigate(`/${selected[0].id}`);
-            }} onSelected
+            }}
             options={hints}
             useCache
             {...props}
