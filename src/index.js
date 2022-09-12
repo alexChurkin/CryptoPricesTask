255d7e6297af4dc91a50abf7bf07312f02d950db
= <churkindev@gmail.com>
2022-09-12 16:27:53 +0300

    React router basename fixed

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/CryptoPricesTask">
       <App />
     </BrowserRouter>
   </Provider>
